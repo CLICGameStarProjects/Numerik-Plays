91eecdd2664a8db51804b61ca3803e51e97c91cc
lucastrg <lucastrg@pop-os.localdomain>
2021-08-06 16:59:03 +0000

    Lots of new stuff, mgba, venv, etc...

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, Blueprint
 import time
 import chartkick
 import random as rd
+import subprocess as sp
 
 
 app = Flask(__name__)
@@ -9,11 +10,14 @@ ck = Blueprint('ck_page', __name__, static_folder=chartkick.js(), static_url_pat
 app.register_blueprint(ck, url_prefix='/ck')
 app.jinja_env.add_extension("chartkick.ext.charts")
 
-BASE_INPUTS = {"up":0, "down":0, "right":0, "left":0}
+BASE_INPUTS = {"up":0, "down":0, "right":0, "left":0, "a":0, "b":0}
 inputs = BASE_INPUTS
 DEMO_VOTING_TIME = 8
 ANAR_VOTING_TIME = 2
 timer=time.perf_counter()
+
+sp.run('mgba /home/lucastrg/Desktop/Pokemon/saphir.gba', shell=True)
+
 
 @app.route('/democracy', methods=['POST', 'GET'])
 def democracy():
@@ -21,10 +25,11 @@ def democracy():
         global inputs
         print(timer-time.perf_counter())
         if (time.perf_counter()-timer)>DEMO_VOTING_TIME:
-                
-                print("Democracy VOTE RESULT", max(inputs.keys(), key=(lambda key: inputs[key])))
+                vote_res = max(inputs.keys(), key=(lambda key: inputs[key]))
+                print("Democracy VOTE RESULT", vote_res)
                 inputs = inputs.fromkeys(inputs, 0) 
                 timer=time.perf_counter()
+                sp.run('echo "$key"', shell=True, env={'key': vote_res})
 
 
         total_votes = sum(inputs.values())
@@ -55,7 +60,9 @@ def anarchy():
                         for val in line:
                                 flat_votes+=[val]
                 if len(flat_votes):
-                        print("Anarchy VOTE RESULT", rd.sample(flat_votes, 1))
+                        vote_res= rd.sample(flat_votes, 1)
+                        print("Anarchy VOTE RESULT",vote_res)
+                        sp.run('echo "$key"', shell=True, env={'key': str(vote_res[0])})
                         inputs = inputs.fromkeys(inputs, 0) 
                         timer=time.perf_counter()
 
@@ -82,9 +89,8 @@ def anarchy():
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-
         if request.method == 'POST':
-                render_template(request.form['mode']+'.html')
+                return redirect('/'+request.form['mode'])
         else:
                 return render_template("index.html")
 
---
+++ trucs.md
@@ -0,0 +1,17 @@
+Trucs à demander a Numerik : un nom de domaine, Nginx Full dans le firewall, un port de notre choix ouvert
+
+
+Comment gérer la répartition des équipes ?
+	Aléatoire ou choix ?
+	Comment gérer la séparation des votes ? Vote en commun ou vote séparé entre les équipes ?
+	
+
+Quels jeux ?
+	Pokemon speedrun d'une arène avec un roster déja fait ?
+	Arene psy de pokemon ruby héhé
+	Ou arene glace de pokemon diamant ?
+	Création de perso skyrim ? un peu chiant à hookup depuis python, il faudrait donner accès à un clavier virtuel depuis le CLI... Un systeme de slider aurait été top, mais ça demande peut être trop de temps de travail...
+	
+	jeu en cooperation ?
+	
+	Quid des droits ? Nintendo serait capable de nous casser les couilles pour ça ? Prévenir les devs dans tous les cas peut être rigolo pour des jeux indés !
